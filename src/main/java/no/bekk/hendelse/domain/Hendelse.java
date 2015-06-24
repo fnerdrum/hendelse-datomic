b2e1cb3e7901d2763daf307529fd035f213ee12b
Frithjof Nerdrum <frithjof.nerdrum@gmail.com>
2015-06-24 13:25:52 +0200

    Hendelse needs an instant

--- src/main/java/no/bekk/hendelse/domain/Hendelse.java
+++ src/main/java/no/bekk/hendelse/domain/Hendelse.java
@@ -1,13 +1,17 @@
 package no.bekk.hendelse.domain;
 
 
+import java.time.Instant;
+
 public class Hendelse {
 
     public final String behandlingsId, type, value;
+    public final Instant time;
 
     public Hendelse(String behandlingsId, String type, String value) {
         this.behandlingsId = behandlingsId;
         this.type = type;
         this.value = value;
+        this.time = Instant.now();
     }
 }
