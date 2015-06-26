16b4f75410aa34cd8b9dda7f6ea48bdb335582fb
Nicklas Utgaard <nicklas.utgaard@bekk.no>
2015-06-26 15:22:09 +0200

    For å sende inn data litt enklere..

---
+++ src/main/resources/senddata.js
@@ -0,0 +1,53 @@
+var prompt = require('sync-prompt').prompt;
+var superagent = require('superagent');
+var format = require('format');
+
+var henvendelser = [];
+
+superagent
+    .get('http://localhost:8080/henvendelse')
+    .end(function (err, res) {
+        if (err) {
+            console.error('Noe gikk feil i uthenting av henvendelser');
+            return 1;
+        }
+        henvendelser = res.body;
+        startPrompt();
+    });
+
+
+function startPrompt(behandlingsId, type, value) {
+    behandlingsId = promptOrDefault('BehandlingsId: ', behandlingsId);
+    type = promptOrDefault('Type: ', type);
+    value = promptOrDefault('Value: ', value);
+
+    var posturl = format('http://localhost:8080/hendelse/receive?behandlingsId=%s&type=%s&value=%s', behandlingsId, type, value);
+
+    superagent
+        .post(posturl)
+        .end(function (err, res) {
+            if (err) {
+                console.log('error while posting');
+            } else {
+                console.log('resp: ', res.body);
+
+                var mer = prompt('Fortsett? [y]')
+                if (mer === '') {
+                    startPrompt(behandlingsId, type, value);
+                }
+            }
+        });
+}
+
+function promptOrDefault(promptStr, defaultValue) {
+    if (defaultValue && defaultValue.length > 0) {
+        var value = prompt(promptStr + ' [' + defaultValue + '] ?');
+        if (value === '') {
+            return defaultValue;
+        } else {
+            value;
+        }
+    } else {
+        return prompt(promptStr);
+    }
+}
