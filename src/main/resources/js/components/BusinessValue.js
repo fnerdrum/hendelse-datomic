0a1b1f15219ae378e32b350e6dbadd7b9a7849cd
Nicklas Utgaard <nutgaard@gmail.com>
2015-07-01 00:40:00 +0200

    Added more business value

--- src/main/resources/js/components/BusinessValue.js
+++ src/main/resources/js/components/BusinessValue.js
@@ -12,9 +12,21 @@ class BusinessValue extends React.Component {
             "color": rate > 30 ? 'red' : 'black'
         };
 
+        let qualifier = 'No ';
+        if (rate >= 30) {
+            qualifier = 'Wow. Such '
+        } else if (rate >= 20) {
+            qualifier = 'Much ';
+        } else if (rate >= 10) {
+            qualifier = 'Moderate ';
+        }
+
+        let muchvalue = qualifier + "Business Value";
+
         return (
             <div className="business-value-container">
                 <span style={style}>{rate}</span>
+                <span style={style}>{muchvalue}</span>
             </div>
         );
     }
